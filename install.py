ac71d28a334df82a1a1a7ae1f72683a10933adfd
dmdhrumilmistry <dhrumilmistry2015@gmail.com>
2021-07-21 16:40:09 +0530

    update ui

--- install.py
+++ install.py
@@ -1,6 +1,5 @@
 #!usr/bin/env python3
-from termux import get_user, generate_passwd, banner, install_req
+from termux import get_user, generate_passwd, install_termux_req
 
-
-install_req()
+install_termux_req()
 generate_passwd(get_user())
--- termux.py
+++ termux.py
@@ -1,16 +1,30 @@
 #!usr/bin/env python3
-
+from prettytable import PrettyTable
 import subprocess
 import colorama
 import re
 from colorama import Style, Fore
-colorama.init(autoreset=True)
 
+
+# for terminal colors
+colorama.init(autoreset=True)
 BRIGHT_WHITE = Style.BRIGHT + Fore.WHITE
 BRIGHT_YELLOW = Style.BRIGHT + Fore.YELLOW
 BRIGHT_RED = Style.BRIGHT + Fore.RED
 BRIGHT_GREEN = Style.BRIGHT + Fore.GREEN
+RESET_COLORS = Style.RESET_ALL
 
+
+# commands menu table
+menu = PrettyTable(['command', 'description'])
+menu.add_row(['start', 'starts SSH server'])
+menu.add_row(['port', 'checks on which port server is running'])
+menu.add_row(['user', 'get username'])
+menu.add_row(['genpass', 'generates new password for user'])
+menu.add_row(['wlan ip', 'get wlan ip of the device'])
+menu.add_row(['connect cmd', 'connect to this using using command printed'])
+menu.add_row(['restart', 'restarts ssh server'])
+menu.add_row(['exit','stops ssh server and exit'])
 
 
 def banner():
@@ -29,36 +43,22 @@ def banner():
 """)
 
 
-def print_menu():
-    subprocess.call("clear", shell=True)
-    banner()
-    print(BRIGHT_WHITE + """
-MENU:
-[1] - Start SSH server
-[2] - check port on which server is running
-[3] - get user
-[4] - get Wlan info (for wlan0 inet) 
-[5] - restart SSH server
-[6] - exit
-------------------------------------------------------------
-[*] How to connect to termux terminal using cmd command:
-1. <user>@<wlan0_inet> -p <port>
-2. Enter password for user
-------------------------------------------------------------
-""")
+def help():
+    print(menu)
 
 
 def get_user():
     return subprocess.check_output("whoami", shell=True).decode('utf-8').strip()
 
 
-def generate_passwd(user):
-    print(BRIGHT_WHITE + "\n[+] Create password for user : ")
+def generate_passwd():
+    user = get_user()
+    print(BRIGHT_WHITE + f"\n[!] Creating new password for user {user} ")
     print(BRIGHT_YELLOW  +"Note: You will be asked to enter password, You must enter the same password while connecting.")
     subprocess.call('passwd', shell=True)
 
 
-def install_req():
+def install_termux_req():
     banner()
     print(BRIGHT_YELLOW + '[+] Installing required packages')
     
@@ -74,39 +74,28 @@ def install_req():
     print(BRIGHT_YELLOW + '[+] Installation completed!!')
 
 
-def start(choice):
-    if choice == 1 and start_ssh():
-        print(Style.BRIGHT + '[+] SSH server has been started successfully!')
-        print(Style.BRIGHT + Fore.YELLOW + 'NOTE: Default Port in most cases 8022')
-    elif choice == 2:
-        check_port()
-    elif choice == 3:
-        print(get_user())
-    elif choice == 4:
-        get_wlan_info()
-    elif choice == 5:
-        restart_ssh()
-    elif choice == 6:
-            exit_program()
-    else:
-        print(Fore.RED + Style.BRIGHT + 'Enter valid choice mate!')
-
-    input("[+] Press any key to continue....")
-
-
 def start_ssh():
-    subprocess.call("sshd", shell=True)
-    return True
+    try:
+        subprocess.call("sshd", shell=True)
+        return True
+    except Exception as e:
+        Exception_Message(e)
 
 
-def check_port():
+def get_ssh_port():
     if start_ssh():
-        nmap_output = subprocess.check_output("nmap localhost", shell=True).decode('utf-8')
-        return nmap_output
+        port_result = subprocess.check_output("nmap localhost", shell=True).decode('utf-8')
+        port_regex = r'(.*)(?:open\s*oa-system)'
+        tcp_port_details = re.search(port_regex, port_result).group(0)
+        tcp_port = re.search(r'\d*', tcp_port_details).group(0)
+        return tcp_port
 
 
-def get_wlan_info():
-    return subprocess.check_output("ifconfig wlan0", shell=True).decode('utf-8')
+def get_wlan_ip():
+    wlan_info = subprocess.check_output("ifconfig wlan0", shell=True).decode('utf-8')
+    wlan_inet_regex = r'(?:inet\s*)(.*)(?:netmask)'
+    wlan_inet_ip = re.search(wlan_inet_regex, wlan_info).group(1).strip()
+    return wlan_inet_ip
 
 
 def kill_ssh():
@@ -137,22 +126,14 @@ def exit_program():
 
 
 def show_connect_command():
-    banner()
     user = get_user()
-
-    wlan_info = get_wlan_info()
-    wlan_inet_regex = r'(?:inet\s*)(.*)(?:netmask)'
-    inet_ip = re.search(wlan_inet_regex, wlan_info).group(1).strip()
-
-    port_result = check_port()
-    port_regex = r'(.*)(?:open\s*oa-system)'
-    tcp_port_details = re.search(port_regex, port_result).group(0)
-    tcp_port = re.search(r'\d*', tcp_port_details).group(0)
+    wlan_ip = get_wlan_ip()
+    ssh_port = get_ssh_port()
 
 
     print(BRIGHT_WHITE + f'[+] USER : {user}')
-    print(BRIGHT_WHITE +f'[+] IP : {inet_ip}')
-    print(BRIGHT_WHITE +f'[+] PORT : {tcp_port}')
+    print(BRIGHT_WHITE +f'[+] IP : {wlan_ip}')
+    print(BRIGHT_WHITE +f'[+] PORT : {ssh_port}')
 
     print(BRIGHT_WHITE +'Connect to this device wirelessly using below command in terminal:')
-    print(BRIGHT_YELLOW + f'ssh {user}@{inet_ip} -p {tcp_port}')
+    print(BRIGHT_YELLOW + f'ssh {user}@{wlan_ip} -p {ssh_port}')
