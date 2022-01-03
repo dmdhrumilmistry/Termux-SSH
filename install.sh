75b076fb34621ca9db23e125def72bf582c1f9c6
dmdhrumilmistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2022-01-03 20:24:09 +0530

    stop execution if exit code is not 0

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo -e "\033[33m[*] Starting installation...\033[0m";apt update -y && apt upgrade -y;apt install python git nmap openssh termux-auth termux-api -y;pip install git+https://github.com/dmdhrumilmistry/Termux-SSH.git;echo "\033[33m[*] Completed... Start Termux-SSH using python -m termux_ssh and use install command to complete setup.\033[0m";
+echo -e "\033[33m[*] Starting installation...\033[0m"
+apt update -y && apt upgrade -y
+apt install python git nmap openssh termux-auth termux-api -y
+pip install git+https://github.com/dmdhrumilmistry/Termux-SSH.git
+echo -e "\033[33m[*] Completed... Start Termux-SSH using python -m termux_ssh and use install command to complete setup.\033[0m";
