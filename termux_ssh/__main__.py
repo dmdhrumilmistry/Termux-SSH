20a35e27056d1a2c1f7dfa5f9dff2f1baf79b132
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2024-06-23 21:22:42 +0530

    port to 3.12

--- termux_ssh/__main__.py
+++ termux_ssh/__main__.py
@@ -1,7 +1,29 @@
-#!usr/bin/env python3
-from termux_ssh.termux import *
 from os import name
 from sys import exit
+
+from .termux import (
+    BRIGHT_RED,
+    BRIGHT_YELLOW,
+    RESET_COLORS,
+    BRIGHT_WHITE,
+    BRIGHT_GREEN,
+    clear_console,
+    cowsay_banner,
+    exception_message,
+    exit_program,
+    get_ssh_port,
+    get_user,
+    generate_passwd,
+    get_tor_hostname,
+    get_wlan_ip,
+    restart_ssh,
+    show_connect_command,
+    start_ssh,
+    start_tor_ssh,
+    stop_tor,
+    install_cmd,
+)
+
 
 if name == 'nt':
     print(BRIGHT_RED + '[-] Termux-SSH is created for Termux Application on Android.')
@@ -16,74 +38,76 @@ print(BRIGHT_YELLOW + "[Note] Termux-SSH only works in Termux Android Applicatio
 
 while True:
     try:
+
         cmd = input(BRIGHT_YELLOW +  "Termux-SSH >> " + RESET_COLORS).lower().strip()
 
-        if cmd == 'exit':
-            exit_program(kill_ssh_server=True, kill_tor_server=True)
+        match cmd:
+            case 'exit':
+                exit_program(kill_ssh_server=True, kill_tor_server=True)
 
-        elif cmd == 'close':
-            exit_program()
+            case 'close':
+                exit_program()
 
-        elif cmd == 'help':
-            help()
+            case 'help':
+                help()
 
-        elif cmd == 'clear':
-            clear_console()
+            case 'clear':
+                clear_console()
         
-        elif cmd == 'install':
-            install_cmd()
+            case 'install':
+                install_cmd()
 
-        elif cmd == 'start':
-            if start_ssh():
-                print(BRIGHT_WHITE + '[*] SSH started successfully')
-            else:
-                print(BRIGHT_RED + '[-] Cannot Start SSH Server')
+            case 'start':
+                if start_ssh():
+                    print(BRIGHT_WHITE + '[*] SSH started successfully')
+                else:
+                    print(BRIGHT_RED + '[-] Cannot Start SSH Server')
 
-        elif cmd == 'port':
-            ssh_port = get_ssh_port()
-            if ssh_port:
-                print(BRIGHT_WHITE + f'[*] PORT : {ssh_port}')
-            else:
-                print(BRIGHT_YELLOW + '[!] Start SSH server before finding the port')
+            case 'port':
+                ssh_port = get_ssh_port()
+                if ssh_port:
+                    print(BRIGHT_WHITE + f'[*] PORT : {ssh_port}')
+                else:
+                    print(BRIGHT_YELLOW + '[!] Start SSH server before finding the port')
 
-        elif cmd == 'user':
-            username = get_user()
-            print(BRIGHT_WHITE + f'[*] USER : {username}')   
+            case 'user':
+                username = get_user()
+                print(BRIGHT_WHITE + f'[*] USER : {username}')   
 
-        elif cmd == 'genpass':
-            generate_passwd()
+            case 'genpass':
+                generate_passwd()
 
-        elif cmd == 'wlanip':
-            wlan_ip = get_wlan_ip()
-            print(BRIGHT_WHITE + f'[*] WLAN IP : {wlan_ip}')
+            case 'wlanip':
+                wlan_ip = get_wlan_ip()
+                print(BRIGHT_WHITE + f'[*] WLAN IP : {wlan_ip}')
 
-        elif cmd == 'conncmd':
-            show_connect_command()
-        
-        elif cmd == 'torssh':
-            start_tor_ssh()
-            print(BRIGHT_GREEN + "[*] SSH can be connected over TOR.")
-        
-        elif cmd == 'torhost':
-            hostname = get_tor_hostname()
-            if hostname != "":
-                print(BRIGHT_GREEN + f"[*] Hostname : {hostname}")
-            else:
-                print(BRIGHT_RED + f"[X] Cannot find hostname, try using install command.")
+            case 'conncmd':
+                show_connect_command()
+            
+            case 'torssh':
+                start_tor_ssh()
+                print(BRIGHT_GREEN + "[*] SSH can be connected over TOR.")
+            
+            case 'torhost':
+                hostname = get_tor_hostname()
+                if hostname != "":
+                    print(BRIGHT_GREEN + f"[*] Hostname : {hostname}")
+                else:
+                    print(BRIGHT_RED + "[X] Cannot find hostname, try using install command.")
 
-        elif cmd == 'torstop':
-            stop_tor()
-            print(BRIGHT_GREEN + "[*] SSH over TOR stopped successfully.")
+            case 'torstop':
+                stop_tor()
+                print(BRIGHT_GREEN + "[*] SSH over TOR stopped successfully.")
 
-        elif cmd == 'restart':
-            restart_ssh()
+            case 'restart':
+                restart_ssh()
 
-        else:
-            print(BRIGHT_RED + '[!] INVALID COMMAND')
+            case _:
+                print(BRIGHT_RED + '[!] INVALID COMMAND')
 
     except (EOFError, KeyboardInterrupt):
         print(BRIGHT_RED + "\n[-] User interruption detected! Services will be running in background")
         exit_program()
 
     except Exception as e:
-        Exception_Message(e)
+        exception_message(e)
